05bc267ac6534644d445d2c04bb06969209e686f
Vitor Quadros <viitorquadros@hotmail.com>
2024-10-21 16:01:07 -0300

    feat (home page): add PatientForm

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,12 @@
+import PatientForm from '@/components/forms/PatientForm';
 import Image from 'next/image';
+import Link from 'next/link';
 
 export default function Home() {
   return (
     <div className="flex h-screen max-h-screen">
+      {/* TODO: OTP Verification / Passkey modal */}
+
       <section className="remove-scrollbar container my-auto">
         <div className="sub-container max-w-[496px]">
           <Image
@@ -12,8 +16,27 @@ export default function Home() {
             alt="Paciente"
             className="mb-12 h-10 w-fit"
           />
+
+          <PatientForm />
+
+          <div className="text-14-regular mt-20 flex justify-between">
+            <p className="justify-items-end text-dark-600 xl:text-left">
+              © 2024 HealthSync
+            </p>
+            <Link href="/?admin=true" className="text-green-500">
+              Admin
+            </Link>
+          </div>
         </div>
       </section>
+
+      <Image
+        src="/assets/images/onboarding-img.png"
+        height={1000}
+        width={1000}
+        alt="Paciente"
+        className="side-img max-w-[50%]"
+      />
     </div>
   );
 }
