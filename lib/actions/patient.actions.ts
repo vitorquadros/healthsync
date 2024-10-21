cf2c2dff9de86858e2dcc04231e7bde5ecaaa34d
Vitor Quadros <viitorquadros@hotmail.com>
2024-10-21 17:36:59 -0300

    feat (actions): create createUser action

---
+++ lib/actions/patient.actions.ts
@@ -0,0 +1,27 @@
+'use server';
+
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { CreateUserParams } from '@/@types';
+import { users } from '../appwrite.config';
+import { ID, Query } from 'node-appwrite';
+import { parseStringify } from '../utils';
+
+export const createUser = async (user: CreateUserParams) => {
+  try {
+    const newUser = await users.create(
+      ID.unique(),
+      user.email,
+      user.phone,
+      undefined,
+      user.name
+    );
+
+    console.log(newUser);
+    return parseStringify(newUser);
+  } catch (error: any) {
+    if (error && error?.code === 409) {
+      const documents = await users.list([Query.equal('email', user.email)]);
+      return documents?.users[0];
+    }
+  }
+};
