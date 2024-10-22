42761acb309eed97b822aef961be3eef2cfe9d3a
Vitor Quadros <viitorquadros@hotmail.com>
2024-10-21 21:07:15 -0300

    feat (constants): add patient info and doctors

--- constants/index.ts
+++ constants/index.ts
@@ -1,1 +1,81 @@
+import { Gender } from '@/@types';
+
 export const GenderOptions = ['Masculino', 'Feminino', 'Outro'];
+
+export const PatientFormDefaultValues = {
+  firstName: '',
+  lastName: '',
+  email: '',
+  phone: '',
+  birthDate: new Date(Date.now()),
+  gender: 'Masculino' as Gender,
+  address: '',
+  occupation: '',
+  emergencyContactName: '',
+  emergencyContactNumber: '',
+  primaryPhysician: '',
+  insuranceProvider: '',
+  insurancePolicyNumber: '',
+  allergies: '',
+  currentMedication: '',
+  familyMedicalHistory: '',
+  pastMedicalHistory: '',
+  identificationType: 'RG',
+  identificationNumber: '',
+  identificationDocument: [],
+  treatmentConsent: false,
+  disclosureConsent: false,
+  privacyConsent: false,
+};
+
+export const IdentificationTypes = [
+  'RG',
+  'Certidão de nascimento',
+  'CNH',
+  'Passaporte',
+];
+
+export const Doctors = [
+  {
+    image: '/assets/images/dr-green.png',
+    name: 'John Green',
+  },
+  {
+    image: '/assets/images/dr-cameron.png',
+    name: 'Leila Cameron',
+  },
+  {
+    image: '/assets/images/dr-livingston.png',
+    name: 'David Livingston',
+  },
+  {
+    image: '/assets/images/dr-peter.png',
+    name: 'Evan Peter',
+  },
+  {
+    image: '/assets/images/dr-powell.png',
+    name: 'Jane Powell',
+  },
+  {
+    image: '/assets/images/dr-remirez.png',
+    name: 'Alex Ramirez',
+  },
+  {
+    image: '/assets/images/dr-lee.png',
+    name: 'Jasmine Lee',
+  },
+  {
+    image: '/assets/images/dr-cruz.png',
+    name: 'Alyana Cruz',
+  },
+  {
+    image: '/assets/images/dr-sharma.png',
+    name: 'Hardik Sharma',
+  },
+];
+
+export const StatusIcon = {
+  scheduled: '/assets/icons/check.svg',
+  pending: '/assets/icons/pending.svg',
+  cancelled: '/assets/icons/cancelled.svg',
+};
