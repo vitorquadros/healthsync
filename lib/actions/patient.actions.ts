220edbc836a1804184c8479b2ea1eb3bbd81ac2f
Vitor Quadros <viitorquadros@hotmail.com>
2024-10-23 14:59:09 -0300

    feat (actions): get patient

--- lib/actions/patient.actions.ts
+++ lib/actions/patient.actions.ts
@@ -75,3 +75,16 @@ export const registerPatient = async ({
     console.error(error);
   }
 };
+
+export const getPatient = async (userId: string) => {
+  try {
+    const patients = await databases.listDocuments(
+      DATABASE_ID!,
+      PATIENT_COLLECTION_ID!,
+      [Query.equal('userId', userId)]
+    );
+    return parseStringify(patients.documents[0]);
+  } catch (error) {
+    console.error(error);
+  }
+};
