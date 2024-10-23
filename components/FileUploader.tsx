ddd5b8be646e64db1fc4e1f18be2a65f3417ad5a
Vitor Quadros <viitorquadros@hotmail.com>
2024-10-23 11:38:01 -0300

    refactor (FileUploader): remove unused property

--- components/FileUploader.tsx
+++ components/FileUploader.tsx
@@ -11,11 +11,14 @@ interface Props {
 }
 
 const FileUploader = ({ files, onChange }: Props) => {
-  const onDrop = useCallback((acceptedFiles: File[]) => {
-    onChange(acceptedFiles);
-  }, []);
+  const onDrop = useCallback(
+    (acceptedFiles: File[]) => {
+      onChange(acceptedFiles);
+    },
+    [onChange]
+  );
 
-  const { getRootProps, getInputProps, isDragActive } = useDropzone({
+  const { getRootProps, getInputProps } = useDropzone({
     onDrop,
   });
 
