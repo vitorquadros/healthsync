5807468b45e5591a11e756be3f861da30f2d391f
Vitor Quadros <viitorquadros@hotmail.com>
2024-10-23 18:16:30 -0300

    feat (success page): implement success page for appointment created successfully

---
+++ app/patients/[userId]/new-appointment/success/page.tsx
@@ -0,0 +1,91 @@
+import { SearchParamProps } from '@/@types';
+import { Button } from '@/components/ui/button';
+import { Doctors } from '@/constants';
+import { getAppointment } from '@/lib/actions/appointment.actions';
+import { formatDateTime } from '@/lib/utils';
+import Image from 'next/image';
+import Link from 'next/link';
+
+const SuccessPage = async ({
+  params: { userId },
+  searchParams,
+}: SearchParamProps) => {
+  const appointmentId = (searchParams?.appointmentId as string) || '';
+  const appointment = await getAppointment(appointmentId);
+
+  const doctor = Doctors.find(
+    (doc) => doc.name === appointment?.primaryPhysician
+  );
+
+  return (
+    <div className="flex h-screen max-h-screen px-[5%]">
+      <div className="success-img">
+        <Link href="/">
+          <Image
+            src="/assets/icons/logo-full.svg"
+            alt="Logo"
+            width={1000}
+            height={1000}
+            className="h-10 w-fit"
+          />
+        </Link>
+
+        <section className="flex flex-col items-center">
+          <Image
+            src="/assets/gifs/success.gif"
+            height={300}
+            width={280}
+            alt="Sucesso"
+          />
+
+          <h2 className="header mb-6 max-w-[600px] text-center">
+            A{' '}
+            <span className="text-green-500">solicitação da sua consulta</span>{' '}
+            foi realizada com sucesso!
+          </h2>
+
+          <p>Você receberá uma confirmação em breve.</p>
+        </section>
+
+        <section className="request-details">
+          <p>Detalhes da solicitação:</p>
+          <div className="flex items-center gap-3">
+            {doctor && (
+              <Image
+                src={doctor.image}
+                alt={doctor.name}
+                width={100}
+                height={100}
+                className="size-6"
+              />
+            )}
+
+            <p className="whitespace-nowrap">Dr. {doctor?.name}</p>
+          </div>
+
+          <div className="flex gap-2">
+            <Image
+              src="/assets/icons/calendar.svg"
+              alt="Calendário"
+              width={24}
+              height={24}
+            />
+            <p>{`${formatDateTime(appointment.schedule).dateOnly} às ${
+              formatDateTime(appointment.schedule).timeOnly
+            }`}</p>
+          </div>
+        </section>
+
+        <Button variant="outline" className="shad-primary-btn" asChild>
+          <Link href={`/patients/${userId}/new-appointment`}>
+            Nova consulta
+          </Link>
+        </Button>
+
+        <p className="copyright">© {new Date().getFullYear()} HealthSync</p>
+      </div>
+    </div>
+  );
+};
+
+export default SuccessPage;
