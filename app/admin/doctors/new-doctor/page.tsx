5dad9fa37281293ce6cab1003f44e18453cc16b8
Vitor Quadros <viitorquadros@hotmail.com>
2024-10-25 11:59:52 -0300

    feat (new doctor page): implement home link and arrow back link

--- app/admin/doctors/new-doctor/page.tsx
+++ app/admin/doctors/new-doctor/page.tsx
@@ -1,18 +1,32 @@
 import DoctorForm from '@/components/forms/DoctorForm';
 import Image from 'next/image';
+import Link from 'next/link';
 
 export default async function NewDoctor() {
   return (
     <div className="flex h-screen max-h-screen">
       <section className="remove-scrollbar container my-auto">
         <div className="sub-container max-w-[860px] flex-1 justify-between">
-          <Image
-            src="/assets/icons/logo-full.svg"
-            height={1000}
-            width={1000}
-            alt="Paciente"
-            className="mb-12 h-10 w-fit"
-          />
+          <div className="flex flex-col gap-6 mb-12">
+            <Link href="/admin/doctors">
+              <Image
+                src="/assets/icons/arrow.svg"
+                height={42}
+                width={42}
+                alt="Voltar"
+              />
+            </Link>
+
+            <Link href="/" className="w-fit">
+              <Image
+                src="/assets/icons/logo-full.svg"
+                height={1000}
+                width={1000}
+                alt="Paciente"
+                className="h-10 w-fit"
+              />
+            </Link>
+          </div>
 
           <DoctorForm />
 
