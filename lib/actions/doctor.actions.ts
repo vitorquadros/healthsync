5527e9c4e7219c15328b41acc764b3aae967ff84
Vitor Quadros <viitorquadros@hotmail.com>
2024-10-25 11:46:54 -0300

    feat (actions): save avatar on storage after create doctor

--- lib/actions/doctor.actions.ts
+++ lib/actions/doctor.actions.ts
@@ -1,21 +1,35 @@
 'use server';
 
-import { ID, Query } from 'node-appwrite';
+import { ID, InputFile, Query } from 'node-appwrite';
 import {
+  BUCKET_ID,
   DATABASE_ID,
   DOCTOR_COLLECTION_ID,
+  ENDPOINT,
+  PROJECT_ID,
   databases,
+  storage,
 } from '../appwrite.config';
 import { parseStringify } from '../utils';
 import { CreateDoctorParams } from '@/@types';
 
 export const createDoctor = async (doctor: CreateDoctorParams) => {
   try {
+    const inputFile = InputFile.fromBlob(
+      doctor.avatar?.get('blobFile') as Blob,
+      doctor.avatar?.get('fileName') as string
+    );
+
+    const file = await storage.createFile(BUCKET_ID!, ID.unique(), inputFile);
+
     const newDoctor = await databases.createDocument(
       DATABASE_ID!,
       DOCTOR_COLLECTION_ID!,
       ID.unique(),
-      doctor
+      {
+        name: doctor.name,
+        avatar: `${ENDPOINT}/storage/buckets/${BUCKET_ID}/files/${file?.$id}/view?project=${PROJECT_ID}`,
+      }
     );
 
     return parseStringify(newDoctor);
