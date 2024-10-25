3999966dd22663b20fbf318cae271f4f3f1580c3
Vitor Quadros <viitorquadros@hotmail.com>
2024-10-25 11:34:13 -0300

    feat (actions): create create doctor action

--- lib/actions/doctor.actions.ts
+++ lib/actions/doctor.actions.ts
@@ -1,12 +1,28 @@
 'use server';
 
-import { Query } from 'node-appwrite';
+import { ID, Query } from 'node-appwrite';
 import {
   DATABASE_ID,
   DOCTOR_COLLECTION_ID,
   databases,
 } from '../appwrite.config';
 import { parseStringify } from '../utils';
+import { CreateDoctorParams } from '@/@types';
+
+export const createDoctor = async (doctor: CreateDoctorParams) => {
+  try {
+    const newDoctor = await databases.createDocument(
+      DATABASE_ID!,
+      DOCTOR_COLLECTION_ID!,
+      ID.unique(),
+      doctor
+    );
+
+    return parseStringify(newDoctor);
+  } catch (error) {
+    console.error(error);
+  }
+};
 
 export const getDoctorsList = async () => {
   try {
